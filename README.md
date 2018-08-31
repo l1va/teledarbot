9c5fbe9c11c5f7e24f6f4cff8c7b0165a5dede5a
l1va <lev_matematik@tut.by>
2018-09-01 00:49:14 +0300

    calendar done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # teledarbot
 Getting events from google calendar by telegram bot in python3
+
+
+Generate credentials.json (https://docs.simplecalendar.io/google-api-key/) and place near (after first login token.json will be generated).
---
+++ get_events.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+from datetime import datetime, timedelta
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+DAYS_DELTA = 1
+
+
+def get_service():
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    return build('calendar', 'v3', http=creds.authorize(Http()))
+
+
+def get_events():
+    service = get_service()
+
+    # To get list of calendars and it's ids uncomment next code
+    # calendars_result = service.calendarList().list().execute()
+    # calendars = calendars_result.get('items', [])
+    # for cal in calendars:
+    #   print(cal['summary'], "id:", cal['id'])
+
+    now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    end_date = (datetime.utcnow() + timedelta(days=DAYS_DELTA)).isoformat() + 'Z'
+    events_result = service.events().list(calendarId='2di64vinvci5d7hb7n13vdv3ok@group.calendar.google.com',
+                                          timeMin=now, timeMax=end_date,
+                                          singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items')
+    res = []
+    for event in events:
+        start = event['start'].get('dateTime')
+        res.append((start, event['summary']))
+    return res
+
+
+if __name__ == '__main__':
+    get_events()
